b3dba67bb6fdc776465a38dba8e4b2a6d8a2603c
Nick <30679225+nickcosmo@users.noreply.github.com>
2021-06-03 15:38:57 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    <div id="left">
+      <app-block height="40%">
+        <div><h1 style="font-size: 200px; margin: 0px">Hi</h1></div>
+        <div><h2 style="font-size: 70px; margin: 0px">I'm Nick!</h2></div>
+      </app-block>
+      <app-block style="margin: 10px 0px" height="10%">
+        <ul
+          style="
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            list-style-type: none;
+            margin: 0px;
+            padding: 0px;
+          "
+        >
+          <li>LinkedIn</li>
+          <li>Email</li>
+          <li>Github</li>
+          <li>codepen</li>
+        </ul>
+      </app-block>
+      <app-block height="50%">My Skills are ...</app-block>
+    </div>
+    <div id="middle">
+      <app-block style="margin-bottom: 5px" height="10%">
+        I am a <span>{{ description }}</span>
+      </app-block>
+      <app-block style="margin-top: 5px" height="90%"
+        >About/Portfolio section</app-block
+      >
+    </div>
+    <div id="right">
+      <app-block style="margin-bottom: 5px" height="40%"
+        >This is what i am listening to ...</app-block
+      >
+      <app-block style="margin-top: 5px" height="60%"
+        >Contact info stuff</app-block
+      >
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppBlock from "./components/AppBlock";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { AppBlock },
+  data() {
+    return {
+      description: null,
+      descriptionArr: [
+        "Web Developer",
+        "Pizza Enthusiast",
+        "Problem Solver",
+        "Snowboarder",
+        "Thinker",
+      ],
+    };
+  },
+  methods: {
+    updateDescription() {
+      let i = 0;
+      this.description = this.descriptionArr[i];
+      setInterval(() => {
+        this.description = this.descriptionArr[i];
+        i++;
+        if (i == this.descriptionArr.length) i = 0;
+      }, 3000);
+    },
+  },
+  created() {
+    this.updateDescription();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#main {
+  display: flex;
+  width: 100%;
+  height: 90vh;
+  justify-content: center;
+}
+
+#left {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0 5px;
+}
+
+#middle {
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 5px;
+}
+
+#right {
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 5px;
 }
 </style>
---
+++ src/components/AppBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :style="{ height: height }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["height"],
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid black;
+  width: 100%;
+  color: black;
+  font-family: sans-serif;
+  text-align: center;
+  align-items: center;
+}
+</style>
