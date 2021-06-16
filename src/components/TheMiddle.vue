690c5c1a9a4533df432b972056ef800314809ebc
Nick <30679225+nickcosmo@users.noreply.github.com>
2021-06-16 19:43:01 +0200

    update to bio

--- src/components/TheMiddle.vue
+++ src/components/TheMiddle.vue
@@ -30,7 +30,7 @@
           <p>
             I am a lot of things, but most of all I consider myself a web
             developer. I am self taught and my primary focus has been on
-            Javascript, Vuejs, and Nodejs. After 2 years of studying, I found a
+            Javascript, Vuejs, and Nodejs. I found a
             creative outlet with programming and I continue to find myself
             captivated by this vast universe of web dev. When I am not coding you
             will probably find me Snowboarding or continuing my search for the
